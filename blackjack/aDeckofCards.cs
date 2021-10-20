7659a381c13895bbbbbb0718dc9af1add11da767
NickH2017 <NicolasHernandezOne@gmail.com>
2021-10-20 11:51:18 -0400

    Project Initialization

---
+++ blackjack/aDeckofCards.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// List of cards, remove card out once
+/// Counter--;
+/// </summary>
+
+namespace blackjack
+{
+    class aDeckofCards
+    {
+    }
+}
